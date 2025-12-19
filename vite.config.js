01d096f070434ac0eacc1a04242ef8b7ad6d9d8b
ravishansenevirathna <travishan55@gmail.com>
2025-12-19 07:41:16 +0530

    add-rover-metrics

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // Base path for GitHub Pages deployment
-  base: '/rover-monitoring/',
+  base: '/battery-monitoring-dashboard/',
 })
